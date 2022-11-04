bde7337568ca90890cb6a353ab957d6159e13137
EmmanuelU <emmanuelurias60@icloud.com>
2022-11-04 12:59:54 -0700

    Made significant advances in JS and fixed some css

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const questionContainer = document.getElementById('question-con')
 
 const welcomeMessage = document.getElementById('start-title')
 
-const questionElement = document.getElementById('questions')
+const questionsElement = document.getElementById('questions')
 const answerBtns = document.getElementById('answer-btns')
 const nextBtn = document.getElementById('nextQuestion-btn btn')
 
@@ -13,6 +13,10 @@ var playerScore = document.getElementById('player-scoreboard')
 let shuffleQuestion, currentQuestion
 
 startTriviaButton.addEventListener('click', startTrivia)
+nextBtn.addEventListener('click', () => {
+    currentQuestion++
+    nextQuestion()
+})
 
 function startTrivia() {
 startTriviaButton.classList.add('hide')
@@ -33,12 +37,13 @@ console.log('test')
 function nextQuestion() {
     resetQuestion()
 
-    showQuestion(shuffleQuestion[currentQuestion])
-    questionElement.innerText = questions.question
+    showQuestion(shuffleQuestion[currentQuestion])    
+
+    console.log('test')
 }
 
 function showQuestion(question) {
-    questionElement.innerText = questions.question
+    questionsElement.innerText = question.question
 
     question.answers.forEach(answer => {
         const button = document.createElement('button')
@@ -52,26 +57,69 @@ function showQuestion(question) {
         button.addEventListener('click', selectAnswer)
         answerBtns.appendChild(button)
     })
+    console.log('test')
 }
 
 function resetQuestion() {
+    clearStatus(document.body)
     nextBtn.classList.add('hide')
     while (answerBtns.firstChild) {
         answerBtns.removeChild(answerBtns.firstChild)
     }
+    console.log('test')
  }
 
-function selectAnswer() {
+function selectAnswer(e) {
+    const selectedBtn = e.target
+    const correct = selectedBtn.dataset.correct
+    setStatus(document.body, correct)
+    Array.from(answerBtns.children).forEach(button => {
+        setStatus(button, button.dataset.correct)
+    })
+    if (shuffleQuestion.length > currentQuestion + 1) {
+        nextBtn.classList.remove('hide')
+    }
+    else {
+        startTriviaButton.innerText = 'Restart'
+        startTriviaButton.classList.remove('hide')
+    }
+    console.log('test')
+}
 
+function setStatus(element, correct) {
+    clearStatus(element)
+    if (correct) {
+        element.classList.add('correct')
+    } 
+    else {
+        element.classList.add('wrong')
+    }
+    console.log('test')
+}
+
+function clearStatus(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+
+    console.log('test')
 }
 
 const questions = [
     {
         question: '...',
         answers: [
-             {text: 'undefined', correct: true },
-             { text: 'undefined', correct: false },
-             { text: 'undefined', correct: false }
+             {text: '....', correct: true },
+             { text: '..', correct: false },
+             { text: '.', correct: false }
+        ]
+    },
+    {
+        question: 'Yo',
+        answers: [
+            {text: 'mama', correct: false},
+            {text: 'mrs. white', correct: true},
+            {text: 'yo', correct: false},
+            {text: 'ho', correct: false}
         ]
     }
 ]
